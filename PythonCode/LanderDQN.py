86b6148b27cbd5be2e925be1b1ddb4fcb5d08418
JonasRosenzweig <Jdrosenzweig@gmail.com>
2020-11-29 11:16:46 +0100

    Fixed LanderDQN replay, fully working now

--- PythonCode/LanderDQN.py
+++ PythonCode/LanderDQN.py
@@ -41,100 +41,85 @@ class DQN:
         predicted_actions = self.model.predict(state)
         return np.argmax(predicted_actions[0])
 
-    def train(self, episodes, num_epochs):
-        for epoch in range(num_epochs):
-            epoch_rewards = []
-            rewards = []
+    def train(self, episodes):
+        for episode in range(episodes):
             steps = 1000
-            for episode in range(episodes):
-                state = env.reset()
-                ep_reward = 0
-                state = np.reshape(state, [1, self.observation_space_dim])
-                episode_frame_count = 0
-                for step in range(steps):
-                    #env.render()
-                    #time.sleep(0.0003)
-                    action = self.get_action(state)
-                    next_state, reward, done, info = env.step(action)
-                    next_state = np.reshape(next_state, [1, self.observation_space_dim])
-                    ep_reward += reward
-                    state = next_state
-                    self.training_frame_count += 1
-                    episode_frame_count += 1
-                    if done: 
-                        break
-                    if self.training_frame_count == 2000000:
-                        break
-                    if self.eps > self.eps_min:
-                        self.eps *= self.eps_decay
-                        
-                rewards.append(ep_reward)
-                print("Training Ep: {}, Ep Reward: {:.2f}, Last Reward: {:.2f}, Total Frames: {}, Ep Frames: {}".format(episode, ep_reward, reward, self.training_frame_count, episode_frame_count))
-            epoch_rewards.append(rewards)
-            env.close()
+            state = env.reset()
+            ep_reward = 0
+            state = np.reshape(state, [1, self.observation_space_dim])
+            episode_frame_count = 0
+            for step in range(steps):
+                #env.render()
+                #time.sleep(0.0003)
+                action = self.get_action(state)
+                next_state, reward, done, info = env.step(action)
+                next_state = np.reshape(next_state, [1, self.observation_space_dim])
+                ep_reward += reward
+                state = next_state
+                self.training_frame_count += 1
+                episode_frame_count += 1
+                if done: 
+                    break
+                if self.training_frame_count == 2000000:
+                    break
+            if self.eps > self.eps_min:
+                self.eps *= self.eps_decay
+                average_reward = np.mean(self.rewards[-100:])
+            self.rewards.append(ep_reward)
+            print("Tr Ep: {}, Ep Reward: {:.2f}, Last Reward: {:.2f}, Total Frames: {}, Frames: {}, Avg Reward: {:.2f}, Eps: {:.2f}".format(episode,
+                                                                                                                                 ep_reward, 
+                                                                                                                                 reward, 
+                                                                                                                                 self.training_frame_count, 
+                                                                                                                                 episode_frame_count, 
+                                                                                                                                 average_reward,
+                                                                                                                                 self.eps))
             
-            for n, rewards in enumerate(epoch_rewards):
-                x = range(len(rewards))
-                cumsum = np.cumsum(rewards)
-                avgs = [cumsum[ep]/(ep+1) if ep<100 else (cumsum[ep]-cumsum[ep-100])/100 for ep in x]
-                plt.plot(x, avgs, label = "Training Epoch {}".format(epoch))
-            plt.legend()
-            plt.xlabel("Episode")
-            plt.ylabel("Average Reward")
-            plt.title("DQN Training Performance")
-            plt.savefig("DQN_Training_Performance")
+            
             
 
     def save(self, name):
         self.model.save(name)
 
-    def test_trained_model(self, trained_model, num_epochs, num_episodes):
-        for epoch in range(num_epochs):
-            epoch_rewards = []
+    def test_trained_model(self, trained_model, num_episodes):
+        for episode in range(num_episodes):
             rewards = []
             steps = 1000
-            for episode in range(num_episodes):
-                trained_state = env.reset()
-                ep_reward = 0
-                observation_space_dim = env.observation_space.shape[0]
-                trained_state = np.reshape(trained_state, [1, observation_space_dim])
-                for step in range(steps):
-                    #env.render()
-                    #time.sleep(0.0003)
-                    trained_action = np.argmax(trained_model.predict(trained_state)[0])
-                    next_state, reward, done, info = env.step(trained_action)
-                    next_state = np.reshape(next_state, [1, observation_space_dim])
-                    trained_state = next_state
-                    ep_reward += reward
-                    if done:
-                        break
-                rewards.append(ep_reward)
-                print("Testing Episode: {}, Episode Reward: {:.2f}, Last Frame Reward: {:.3f}".format(episode, ep_reward, reward))
-            epoch_rewards.append(rewards)
-            env.close()
+            trained_state = env.reset()
+            ep_reward = 0
+            observation_space_dim = env.observation_space.shape[0]
+            trained_state = np.reshape(trained_state, [1, observation_space_dim])
+            for step in range(steps):
+                #env.render()
+                #time.sleep(0.0003)
+                trained_action = np.argmax(trained_model.predict(trained_state)[0])
+                next_state, reward, done, info = env.step(trained_action)
+                next_state = np.reshape(next_state, [1, observation_space_dim])
+                trained_state = next_state
+                ep_reward += reward
+                if done:
+                    break
+                average_reward = np.mean(rewards[-100:])
+            rewards.append(ep_reward)
+            print("Tr Ep: {}, Ep Reward: {:.2f}, Last Reward: {:.2f}, Avg Reward: {:.2f}".format(episode,
+                                                                                             ep_reward, 
+                                                                                             reward, 
+                                                                                             average_reward))
 
-            for n, rewards in enumerate(epoch_rewards):
-                x = range(len(rewards))
-                cumsum = np.cumsum(rewards)
-                avgs = [cumsum[ep]/(ep+1) if ep<100 else (cumsum[ep]-cumsum[ep-100])/100 for ep in x]
-                plt.plot(x, avgs, label="Testing Curve")
-            plt.legend()
-            plt.title("DQN Performance after Training")
-            plt.xlabel("Episode")
-            plt.ylabel("Average Reward")
-            plt.savefig("DQN_Performance_after_Training.png")
+            
 
 if __name__ == '__main__':
     env = gym.make('LunarLander-v2')
-    lr = 1
+    env.seed(21)
+    np.random.seed(21)
+    lr = 0.001
     eps = 1.0
     eps_decay = 0.995
-    training_episodes = 3
+    training_episodes = 200
     model = DQN(env, lr, eps, eps_decay)
-    model.train(training_episodes, num_epochs=2)
+    model.train(training_episodes)
     model.save("simpleDQN_trained_model.h5")
     trained_model = load_model("simpleDQN_trained_model.h5")
-    model.test_trained_model(trained_model, num_epochs = 1, num_episodes=2)
+    model.test_trained_model(trained_model, num_episodes=10)
 
 
 
--- PythonCode/LanderDQN_replay.py
+++ PythonCode/LanderDQN_replay.py
@@ -1,7 +1,9 @@
 import gym
 import random
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import deque
 
 from keras import Sequential
 from keras.layers import Dense
@@ -10,31 +12,33 @@ from keras.optimizers import Adam
 from keras.losses import mean_squared_error
 from keras.models import load_model
 
-from collections import deque
-
 
 class DQN:
-    def __init__(self, env, lr, eps, eps_decay):
+    def __init__(self, env, lr, gamma, eps, eps_decay):
         self.env = env
         self.action_space = env.action_space
         self.observation_space = env.observation_space
         self.action_space_dim = self.action_space.n
         self.observation_space_dim = self.observation_space.shape[0]
         self.lr = lr
+        self.gamma = gamma
         self.eps = eps
         self.eps_decay = eps_decay
         self.eps_min = 0.01
         self.rewards = []
+        self.trained_rewards = []
         self.training_frame_count = 0
-        self.model = self.initialize_model()
         self.replay_memory_buffer = deque(maxlen=500000)
         self.batch_size = 64
-        self.counter_for_replay = 0
+        self.model = self.initialize_model()
+        self.counter = 0
+        self.average_rewards = []
+        self.average_rewards_trained = []
 
     def initialize_model(self):
         model = Sequential()
-        model.add(Dense(32, input_dim = self.observation_space_dim, activation=relu))
-        model.add(Dense(16, activation=relu))
+        model.add(Dense(512, input_dim = self.observation_space_dim, activation=relu))
+        model.add(Dense(256, activation=relu))
         model.add(Dense(self.action_space_dim, activation=linear))
         model.compile(loss=mean_squared_error, optimizer=Adam(lr=self.lr))
         print(model.summary())
@@ -45,7 +49,7 @@ class DQN:
             return random.randrange(self.action_space_dim)
         predicted_actions = self.model.predict(state)
         return np.argmax(predicted_actions[0])
-
+    
     def add_to_replay_memory(self, state, action, reward, next_state, done):
         self.replay_memory_buffer.append((state, action, reward, next_state, done))
 
@@ -56,7 +60,7 @@ class DQN:
             return
 
         # Early Stopping
-        if np.mean(self.rewards_list[-10:]) > 180:
+        if np.mean(self.rewards[-10:]) > 180:
             return
 
         random_sample = self.get_random_sample_from_replay_mem()
@@ -82,112 +86,122 @@ class DQN:
         random_sample = random.sample(self.replay_memory_buffer, self.batch_size)
         return random_sample
 
-    def update_counter(self):
-        self.counter_for_replay += 1
-        step_size = 5
-        self.counter_for_replay = self.counter_for_replay % step_size
-
-    def train(self, episodes, num_epochs):
-        for epoch in range(num_epochs):
-            epoch_rewards = []
-            rewards = []
+    def train(self, episodes):
+        for episode in range(episodes):
             steps = 1000
-            for episode in range(episodes):
-                state = env.reset()
-                ep_reward = 0
-                state = np.reshape(state, [1, self.observation_space_dim])
-                episode_frame_count = 0
-                for step in range(steps):
-                    #env.render()
-                    #time.sleep(0.0003)
-                    action = self.get_action(state)
-                    next_state, reward, done, info = env.step(action)
-                    next_state = np.reshape(next_state, [1, self.observation_space_dim])
-                    ep_reward += reward
-                    state = next_state
-                    self.update_counter()
-                    self.learn_and_update_weights_by_reply() 
-                    self.training_frame_count += 1
-                    episode_frame_count += 1
-                    if done: 
-                        break
-                    if self.training_frame_count == 2000000:
-                        break
-                    if self.eps > self.eps_min:
-                        self.eps *= self.eps_decay
-
-                    last_rewards_mean = np.mean(self.rewards[-100:])
-                    if last_rewards_mean > 200:
-                        print("DQN Training Complete...")
-                        break
-                        
-                rewards.append(ep_reward)
-                print("Training Ep: {}, Ep Reward: {:.2f}, Last Reward: {:.2f}, Total Frames: {}, Ep Frames: {}".format(episode, ep_reward, reward, self.training_frame_count, episode_frame_count))
-            epoch_rewards.append(rewards)
-            env.close()
+            state = env.reset()
+            ep_reward = 0
+            state = np.reshape(state, [1, self.observation_space_dim])
+            episode_frame_count = 0
+            for step in range(steps):
+                #env.render()
+                #time.sleep(0.0003)
+                exploit_action = self.get_action(state)
+                next_state, reward, done, info = env.step(exploit_action)
+                next_state = np.reshape(next_state, [1, self.observation_space_dim])
+                ep_reward += reward
+                self.training_frame_count += 1
+                episode_frame_count += 1
+                self.add_to_replay_memory(state, exploit_action, reward, next_state, done)
+                state = next_state
+                self.update_counter()
+                self.learn_and_update_weights_by_reply()
+                if done: 
+                    break
+            if self.eps > self.eps_min:
+                self.eps *= self.eps_decay
             
-            for n, rewards in enumerate(epoch_rewards):
-                x = range(len(rewards))
-                cumsum = np.cumsum(rewards)
-                avgs = [cumsum[ep]/(ep+1) if ep<100 else (cumsum[ep]-cumsum[ep-100])/100 for ep in x]
-                plt.plot(x, avgs, label = "Training Epoch {}".format(epoch))
-            plt.legend()
-            plt.xlabel("Episode")
-            plt.ylabel("Average Reward")
-            plt.title("DQN Training Performance")
-            plt.savefig("DQN_Training_Performance")
+            average_reward = np.mean(self.rewards[-100:])
+            if average_reward > 200:
+                break
+            self.rewards.append(ep_reward)
+            self.average_rewards.append(average_reward)
+            
+            print("Tr Ep: {}, Ep Reward: {:.2f}, Last Reward: {:.2f}, Total Frames: {}, Frames: {}, Avg Reward: {:.2f}, Eps: {:.2f}".format(episode,
+                                                                                                                                            ep_reward, 
+                                                                                                                                     reward, 
+                                                                                                                                     self.training_frame_count, 
+                                                                                                                                     episode_frame_count, 
+                                                                                                                                     average_reward,
+                                                                                                                                     self.eps))
+            if episode % 9 == 0:
+                plt.plot(self.average_rewards)
+                plt.plot(self.rewards)
+                plt.title("DQN Replay Training Performance Curve")
+                plt.xlabel("Episode")
+                plt.ylabel("Rewards")
+                plt.show()
+                
+                
+        env.close()
+        plt.savefig("DQN_Replay_Training_Performance_Curve")
+        
+        
+        
+       
+        
+            
+            
             
 
     def save(self, name):
         self.model.save(name)
+        
+    def update_counter(self):
+        self.counter += 1
+        step_size = 5
+        self.counter = self.counter % step_size
 
-    def test_trained_model(self, trained_model, num_epochs, num_episodes):
-        for epoch in range(num_epochs):
-            epoch_rewards = []
-            rewards = []
+    def test_trained_model(self, trained_model, num_episodes):
+        for episode in range(num_episodes):
             steps = 1000
-            for episode in range(num_episodes):
-                trained_state = env.reset()
-                ep_reward = 0
-                observation_space_dim = env.observation_space.shape[0]
-                trained_state = np.reshape(trained_state, [1, observation_space_dim])
-                for step in range(steps):
-                    #env.render()
-                    #time.sleep(0.0003)
-                    trained_action = np.argmax(trained_model.predict(trained_state)[0])
-                    next_state, reward, done, info = env.step(trained_action)
-                    next_state = np.reshape(next_state, [1, observation_space_dim])
-                    trained_state = next_state
-                    ep_reward += reward
-                    if done:
-                        break
-                rewards.append(ep_reward)
-                print("Testing Episode: {}, Episode Reward: {:.2f}, Last Frame Reward: {:.3f}".format(episode, ep_reward, reward))
-            epoch_rewards.append(rewards)
-            env.close()
+            trained_state = env.reset()
+            ep_reward = 0
+            observation_space_dim = env.observation_space.shape[0]
+            trained_state = np.reshape(trained_state, [1, observation_space_dim])
+            for step in range(steps):
+                #env.render()
+                #time.sleep(0.0003)
+                trained_action = np.argmax(trained_model.predict(trained_state)[0])
+                next_state, reward, done, info = env.step(trained_action)
+                next_state = np.reshape(next_state, [1, observation_space_dim])
+                trained_state = next_state
+                ep_reward += reward
+                if done:
+                    break
+            
+            average_reward_trained = np.mean(self.trained_rewards[-100:])
+            self.trained_rewards.append(ep_reward)
+            self.average_rewards_trained.append(average_reward_trained)
+            
+            print("Tr Ep: {}, Ep Reward: {:.2f}, Last Reward: {:.2f}, Avg Reward: {:.2f}".format(episode,
+                                                                                             ep_reward, 
+                                                                                             reward, 
+                                                                                             average_reward_trained))
+        env.close()
+        plt.plot(self.average_rewards_trained)
+        plt.plot(self.trained_rewards)
+        plt.title("DQN Replay Trained Performance Curve")
+        plt.xlabel("Episode")
+        plt.ylabel("Rewards")
+        plt.show()
 
-            for n, rewards in enumerate(epoch_rewards):
-                x = range(len(rewards))
-                cumsum = np.cumsum(rewards)
-                avgs = [cumsum[ep]/(ep+1) if ep<100 else (cumsum[ep]-cumsum[ep-100])/100 for ep in x]
-                plt.plot(x, avgs, label="Testing Curve")
-            plt.legend()
-            plt.title("DQN Performance after Training")
-            plt.xlabel("Episode")
-            plt.ylabel("Average Reward")
-            plt.savefig("DQN_Performance_after_Training.png")
+            
 
 if __name__ == '__main__':
     env = gym.make('LunarLander-v2')
-    lr = 0.1
+    env.seed(21)
+    np.random.seed(21)
+    lr = 0.001
     eps = 1.0
     eps_decay = 0.995
+    gamma = 0.99
     training_episodes = 200
-    model = DQN(env, lr, eps, eps_decay)
-    model.train(training_episodes, num_epochs=5)
-    model.save("simpleDQN_trained_model.h5")
-    trained_model = load_model("simpleDQN_trained_model.h5")
-    model.test_trained_model(trained_model, num_epochs = 1, num_episodes=100)
+    model = DQN(env, lr, gamma, eps, eps_decay)
+    model.train(training_episodes)
+    model.save("replay_DQN_trained_model3.h5")
+    trained_model = load_model("replay_DQN_trained_model3.h5")
+    model.test_trained_model(trained_model, num_episodes=30)
 
 
 
