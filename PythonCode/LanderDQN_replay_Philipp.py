ac5f328800bfa909ca893f2e38afbdb8ea9e0afe
SirBuster33 <sirbuster333@gmail.com>
2020-11-30 14:17:51 +0100

    Added more stuff in PhiDQN

--- PythonCode/LanderDQN_replay_Philipp.py
+++ PythonCode/LanderDQN_replay_Philipp.py
@@ -70,7 +70,8 @@ class DQN:
     def initialize_model(self):
         model = Sequential()
         
-        # Input layer
+        
+        # Input layer (based on observation space of the environment)
         model.add(Dense(512, input_dim = self.num_observation_space, activation = relu))
         model.add(Dropout(0.2))
         
@@ -78,8 +79,67 @@ class DQN:
         model.add(Dense(256, activation = relu))
         model.add(Dropout(0.1))
         
-        # Output layer
+        # Output layer (based on action space of the environment)
         model.add(Dense(self.num_action_space, activation = linear))
         
         
+        # Compile the model giving the loss and the optimizer as an argument.
+        model.compile(loss = mean_squared_error, optimizer = Adam (lr = self.lr))
+        
+        # Prints out the stats of the model to give an overview over what was just created.
+        print(model.summary())
+        
+        
+        return model
 
+    # Decide whether to take an exploratory or exploitative action.
+    def get_action(self, state):
+        
+        # Based on a random number 0 <= n <= 1, if n smaller than the current epsilon e, select random action based on the action space of the environment.
+        if np.random.rand() < self.epsilon:
+            return random.randrange(self.num_action_space)
+
+        # Otherwise let the model decide the best action in the current environment state based on the momentary policy.
+        predicted_actions = self.model.predict(state)
+        
+        # Return the action to be taken in the current state.
+        return np.argmax(predicted_actions[0])
+    
+    def learn_and_update_weights_by_reply(self):
+        
+        # replay_memory_buffer size check (Needs rewording / more understanding)
+        if len(self.replay_memory_buffer) < self.batch_size or self.counter != 0:
+            return
+        
+        # If the model has been completing the task with a desirable reward for a while, stop it to prevent it from overfitting.
+        if np.mean(self.rewards_list[-10]) > 180:
+            return
+        
+        # Choose a random past experience from the replay memory
+        random_sample = self.get_randomo_sample_from_replay_mem()
+        # Convert the chosen experience's attributes to the needed parameters (state, action, etc.)
+        states, actions, rewards, next_states, done_list = self.get_attributes_from_sample(random_sample)
+        
+        # Update the rewards based on the discount factor (gamma) influencing the next set of states. (Needs revising / more understanding)
+        targets = rewards + self.gamma * (np.amax(self.model.predict_on_batch(next_states), axis = 1)) * (1 - done_list)
+        target_vec = self.model.predict_on_batch(states)
+        indexes = np.array([i for i in range(self.batch_size)])
+        target_vec[[indexes], [actions]] = targets
+        
+        # Adjusts the policy based on states, target vectors and other things (needs more understanding)
+        self.model.fit(states, target_vec, epochs = 1, verbose = 0)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
