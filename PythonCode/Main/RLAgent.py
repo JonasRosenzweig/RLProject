c9bbdf32b106dcbdf9115a4798a4e8b1dca93a09
unknown <sirbuster333@gmail.com>
2020-12-14 18:20:56 +0100

    Documentation start

--- PythonCode/Main/RLAgent.py
+++ PythonCode/Main/RLAgent.py
@@ -1,4 +1,38 @@
 class RLAgent:
+    """
+    A class used to build RL algorithms
+    
+    This class serves as a superclass to build more specific reinforcement 
+    learning (RL) algorithms and provides the fundamental parameters found in
+    all RL algorithms.
+    
+    Attributes
+    ---------
+    env : env
+        The environment of a RL algorithm
+        
+    action_space : space
+        The environment's action space
+        
+    observation_space : space
+        The environment's observation space
+        
+    training_episodes : int
+        Maximum number of training episodes
+        
+    testing_episodes : int
+        Maximum number of testing episodes
+        
+    frames : int
+    
+    test_episode_rewards : list of ints
+    
+    test_average_rewards : list of ints
+    
+    training_episode_rewards : list of ints
+    
+    training_average_rewards : list of ints
+    """
     def __init__(self, env, training_episodes, testing_episodes, frames):
         self.env = env
         self.action_space = env.action_space
