8eb611bb78976a153f3d2977f73cad01922e001f
SirBuster33 <sirbuster333@gmail.com>
2020-12-18 02:58:55 +0100

    Push it good, Philipp

--- PythonCode/Main/QLearning.py
+++ PythonCode/Main/QLearning.py
@@ -27,11 +27,17 @@ class QAgent(RLAgent):
     gamma : float
         The discount factor for future rewards.
         
-    min_learning_rate : float
-        The minimum learning rate.
+    learning_rate : float
+        The learning rate.
+        
+    epsilon : float
+        The epsilon.
         
     epsilon_min : float
         The minimum epsilon value.
+        
+    epsilon_decay : float
+        The epsilon decay.
         
     divisor : int or float
         Number used in learning rate decay.
@@ -59,23 +65,18 @@ class QAgent(RLAgent):
     update_Q(state, action, reward, next_state, learning_rate)
         Updates the state-action pairs and Q values in the Q-table.
     
-    get_epsilon(episode)
-        Decays and returns the epsilon.
-    
-    get_learning_rate(episode)
-        Decays and returns the learning rate.
-    
     run()
         Loop to train the Q-agent.
     """
     
-    def __init__(self, env, gamma, min_learning_rate, epsilon_min, divisor,
+    def __init__(self, env, gamma, learning_rate, epsilon, epsilon_min, divisor,
                  buckets, training_episodes, testing_episodes, frames):
         
         RLAgent.__init__(self, env, training_episodes, testing_episodes, frames)
         self.env = env
         self.gamma = gamma
-        self.min_learning_rate = min_learning_rate
+        self.learning_rate = learning_rate
+        self.epsilon = epsilon
         self.epsilon_min = epsilon_min
         self.divisor = divisor 
         self.buckets = (3,3,6,6,)
@@ -111,34 +112,24 @@ class QAgent(RLAgent):
                 reward + self.gamma * np.max(
                 self.Q[next_state]) - self.Q[state][action])
         
-    def get_epsilon(self, episode):
         return max(self.epsilon_min, min(1.0,
                      1.0 - math.log10((episode + 1)/self.divisor)))
     
-    def get_learning_rate(self, episode):
-        return max(self.min_learning_rate, min(1.0,
-                     1.0 - math.log10((episode + 1) / self.divisor)))
-    
-    # def save_Q_table(self, state, action, reward, nexct_state, done, info):
-
     def run(self):
         print("running")
-        #wandb.init(project="QLearning", name = "Performance")
         for episode in range(self.training_episodes):
             
             episode_reward = 0
             discretized_state = self.discretize(self.env.reset())
-            learning_rate = self.get_learning_rate(episode)
-            epsilon = self.get_epsilon(episode)
             done = False
             
             while not done:
                 
-                action = self.get_action(discretized_state, epsilon)
+                action = self.get_action(discretized_state, self.epsilon)
                 state, reward, done, info = self.env.step(action)
                 next_state = self.discretize(state)
                 self.update_Q(discretized_state, action, reward, next_state,
-                              learning_rate)
+                              self.learning_rate)
                 discretized_state = next_state
                 # env.render()
                 episode_reward += reward
@@ -147,17 +138,14 @@ class QAgent(RLAgent):
             if average_reward > 200:
                 break
             
+            if self.epsilon > self.epsilon_min:
+                self.epsilon *= self.epsilon_decay
+            
             self.training_episode_rewards.append(episode_reward)
             self.training_average_rewards.append(average_reward)
-            #wandb.log({'reward': average_reward, 'epsilon' : epsilon}, step = episode)
             print("Episode: {}, total_reward: {:.2f}, Epsilon: {:.2f}".format(
-                episode, episode_reward, epsilon))
+                episode, episode_reward, self.epsilon))
             
             
         self.env.close()
         
-
-        
-                
-            
-            
