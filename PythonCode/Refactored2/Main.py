418fd590df5390898dcc81d96efe1c1d2dd05e69
SirBuster33 <sirbuster333@gmail.com>
2021-01-14 14:07:30 +0100

    main change

--- PythonCode/Refactored2/Main.py
+++ PythonCode/Refactored2/Main.py
@@ -5,7 +5,7 @@ import wandb
 
 from QAgent import QAgent
 from DQAgent import DQAgent
-from Run import Train, Test
+from Run import Run
 from Experiments import  gammaExperiment, learningRateExperiment, epsilonDecayExperiment
 
 if __name__ == '__main__':
@@ -48,6 +48,7 @@ if __name__ == '__main__':
     goal = 200
     min_reward = -300
     
+    
     name = "LR_{}_LS_{}_BS_{}_MS_{}_Timestamp_{}".format(learning_rate, layer_size, batch_size, memory_size, int(time.time()))
     # name = agent1.name
     
@@ -72,17 +73,20 @@ if __name__ == '__main__':
     
     # agent2 = QAgent(env, config)
     agent1 = DQAgent(env, config)
+    # Train(agent1, agent1_run_config, goal, min_reward)
     
-    agent1_run_config = {"training_episodes": training_episodes, "steps": steps, "render": render, 
+    
+    agent1_run_config = {"experiment_episodes": experiment_episodes, "training_episodes": training_episodes, "testing_episodes": testing_episodes, "steps": steps, "render": render, 
                     "early_stop": early_stop, "episode_time_limit": episode_time_limit, "goal": goal, "min_reward": min_reward}
-    Train(agent1, agent1_run_config, goal, min_reward)
+    Run = Run(agent1, agent1_run_config)
+    Run.train()
     
-    gamma_experiment_config = {"experiment_episodes": experiment_episodes, "gammas": gammas}
-    gammaExperiment(agent1, gamma_experiment_config)
+    # gamma_experiment_config = {"experiment_episodes": experiment_episodes, "gammas": gammas}
+    # gammaExperiment(agent1, gamma_experiment_config)
     
-    learning_rate_experiment_config = {"experiment_episodes": experiment_episodes, "learning_rates": learning_rates}
-    learningRateExperiment(agent1, learning_rate_experiment_config)
+    # learning_rate_experiment_config = {"experiment_episodes": experiment_episodes, "learning_rates": learning_rates}
+    # learningRateExperiment(agent1, learning_rate_experiment_config)
     
-    epsilon_decay_experiment_config = {"experiment_episodes": experiment_episodes, "epsilon_decays": epsilon_decay}
-    epsilonDecayExperiment(agent1, epsilon_decay_experiment_config)
+    # epsilon_decay_experiment_config = {"experiment_episodes": experiment_episodes, "epsilon_decays": epsilon_decay}
+    # epsilonDecayExperiment(agent1, epsilon_decay_experiment_config)
     
