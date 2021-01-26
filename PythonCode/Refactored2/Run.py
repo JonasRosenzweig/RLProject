c0287541e00a695978bf10e35f49bde9d168d1ff
SirBuster33 <sirbuster333@gmail.com>
2021-01-26 13:16:36 +0100

    Commented out classes in Run

--- PythonCode/Refactored2/Run.py
+++ PythonCode/Refactored2/Run.py
@@ -34,6 +34,7 @@ class Run:
                 self.state = self.Agent.discretize(state)
             elif self.Agent.config.name == "DQAgent":
                 state = np.reshape(state, [1, self.Agent.observation_space_size])
+                print("shape of state is: ", state.shape)
                 
             for step in range(self.run_config["steps"]):
                 action = self.Agent.act(state)
@@ -143,7 +144,7 @@ Last Frame Reward: {:.2f}\t|| Average Reward: {:.2f}"""
                 self.epsilon_decay_rewards.append(self.training_episode_rewards)   
         
         
-    
+"""    
 class Train(Run):
     def __init__(self, Agent, run_config, goal, min_reward):
         print("Training {}".format(Agent.name))
@@ -203,12 +204,12 @@ class Train(Run):
             self.training_episode_rewards.append(episode_reward)
             self.training_average_rewards.append(average_reward)
             wandb.log({'average reward': average_reward, 'last reward': reward, 'epsilon': Agent.config.epsilon, 'episode': episode }, step=episode)
-            print("""Episode: {}\t\t\t|| Episode Reward: {:.2f}
+            print(""""""Episode: {}\t\t\t|| Episode Reward: {:.2f}
 Last Frame Reward: {:.2f}\t|| Average Reward: {:.2f}\t|| Epsilon: {:.2f}
-Frames this episode: {}\t\t|| Total Frames trained: {}\n"""
+Frames this episode: {}\t\t|| Total Frames trained: {}\n""""""
                 .format(episode, episode_reward, reward, average_reward, Agent.config.epsilon, episode_frame_count, self.run_frame_count))
-        Agent.env.close()
-        
+        Agent.env.close()"""
+"""        
 class Test(Train):
     def __init__(self, Agent, run_config, trained_model):
         print("Testing {}".format(Agent.name))
@@ -239,8 +240,8 @@ class Test(Train):
             average_reward_trained = np.mean(self.trained_rewards[-100:])
             self.test_episode_rewards.append(episode_reward)
             self.test_average_rewards.append(average_reward_trained)
-            print("""Episode: {}\t\t\t|| Episode Reward: {:.2f}\
-Last Frame Reward: {:.2f}\t|| Average Reward: {:.2f}"""
+            print(""""""Episode: {}\t\t\t|| Episode Reward: {:.2f}\
+Last Frame Reward: {:.2f}\t|| Average Reward: {:.2f}""""""
               .format(episode, episode_reward, reward, average_reward_trained))
         Agent.env.close()
 
@@ -276,4 +277,4 @@ class epsilonDecayExperiment(Run):
                 print("Training for Learning Rate: {}".format(epsilon_decay))
                 Train(Agent, run_config)
                 self.epsilon_decay_rewards.append(self.training_episode_rewards)                
-    
+"""    
